0a9af01e7d17d0fea60c3a00d5b9689a90446e5e
bscout9956 <bs.blackscout.bs@gmail.com>
2025-03-21 18:29:46 -0300

    Add missing imports

--- CodeWalker.Core/GameFiles/RpfFile.cs
+++ CodeWalker.Core/GameFiles/RpfFile.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
@@ -6,6 +7,7 @@ using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeWalker.Utils;
 
 namespace CodeWalker.GameFiles
 {
